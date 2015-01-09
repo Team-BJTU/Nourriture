7303728ac8f4e07c1dce85a79b1dd692bb33511e
anteno_y <anteno_y@epitech.eu>
2015-01-10 00:01:25 +0800

    [FEATURE] New Login Activity

--- Nourriture_app/Nourriture/app/src/main/java/nourriture/teambjtu/com/nourriture/CreateAccountActivity.java
+++ Nourriture_app/Nourriture/app/src/main/java/nourriture/teambjtu/com/nourriture/CreateAccountActivity.java
@@ -1,11 +1,30 @@
 package nourriture.teambjtu.com.nourriture;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CreateAccountActivity extends ActionBarActivity implements View.OnClickListener {
@@ -16,6 +35,7 @@ public class CreateAccountActivity extends ActionBarActivity implements View.OnC
     public EditText UserNameEditText;
     public EditText PasswordEditText;
     public EditText RetypePasswordEditText;
+    public Button RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,7 @@ public class CreateAccountActivity extends ActionBarActivity implements View.OnC
         UserNameEditText = (EditText) findViewById(R.id.UserNameEditText);
         PasswordEditText = (EditText) findViewById(R.id.PasswordEditText);
         RetypePasswordEditText = (EditText) findViewById(R.id.RetypePasswordEditText);
+        RegisterButton = (Button) findViewById(R.id.RegisterButton);
 
         FirstNameEditText.setOnClickListener(new View.OnClickListener(){
 
@@ -76,8 +97,79 @@ public class CreateAccountActivity extends ActionBarActivity implements View.OnC
                 RetypePasswordEditText.setText("");
             }
         });
+
+        /*RegisterButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Register register = new Register();
+                register.execute();
+            }
+        });*/
     }
 
+    /*private class Register extends AsyncTask<Void, Integer, Void>
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            Log.v("PreExecute", "On PreExcute");
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.v("OnProgressUpdate", "On ProgressUpdate");
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.v("DoInBackGround", "On DoInBackGround");
+            HttpPost httppost = new HttpPost("ec2-54-64-190-134.ap-northeast-1.compute.amazonaws.com:3000/signup");
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("First Name", FirstNameEditText.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("Last Name", LastNameEditText.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("Email", EmailEditText.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("Username", UserNameEditText.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("Password", PasswordEditText.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("Retype Password", RetypePasswordEditText.getText().toString()));
+
+            try {
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            HttpClient httpclient = new DefaultHttpClient();
+            try {
+                httpclient.execute(httppost);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            HttpResponse response= null;
+            try {
+                response = httpclient.execute(httppost);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert response != null;
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                String s = reader.readLine();
+                Log.v("Response", s);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Toast.makeText(getApplicationContext(), "Attempt to Register is over !", Toast.LENGTH_LONG).show();
+        }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Nourriture_app/Nourriture/app/src/main/java/nourriture/teambjtu/com/nourriture/MainActivity.java
+++ Nourriture_app/Nourriture/app/src/main/java/nourriture/teambjtu/com/nourriture/MainActivity.java
@@ -1,19 +1,43 @@
 package nourriture.teambjtu.com.nourriture;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public EditText UsernameEditText;
     public EditText PasswordEditText;
+    public Button LoginButton;
 
+    public void LogIn(View view) {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+    }
 
     public void CreateAnAccount(View view) {
         // Do something in response to button
@@ -33,6 +57,7 @@ public class MainActivity extends ActionBarActivity {
 
         UsernameEditText = (EditText) findViewById(R.id.UsernameEditText);
         PasswordEditText = (EditText) findViewById(R.id.PasswordEditText);
+        LoginButton = (Button) findViewById(R.id.LoginButton);
 
         UsernameEditText.setOnClickListener(new View.OnClickListener(){
 
@@ -49,8 +74,74 @@ public class MainActivity extends ActionBarActivity {
                 PasswordEditText.setText("");
             }
         });
+
+        /*LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login login = new Login();
+                login.execute();
+            }
+        });*/
     }
 
+   /* private class Login extends AsyncTask<Void, Integer, Void>
+    {
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            Log.v("PreExecute", "On PreExcute");
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.v("OnProgressUpdate", "On ProgressUpdate");
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.v("DoInBackGround", "On DoInBackGround");
+            HttpPost httppost = new HttpPost("ec2-54-64-190-134.ap-northeast-1.compute.amazonaws.com:3000/SignIn");
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("Login", UsernameEditText.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("Password", PasswordEditText.getText().toString()));
+            try {
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            HttpClient httpclient = new DefaultHttpClient();
+            try {
+                httpclient.execute(httppost);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            HttpResponse response= null;
+            try {
+                response = httpclient.execute(httppost);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert response != null;
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                String s = reader.readLine();
+                Log.v("Response", s);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Toast.makeText(getApplicationContext(), "Attempt to login is over !", Toast.LENGTH_LONG).show();
+        }
+    } */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
